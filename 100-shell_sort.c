a086a58c1f35f17235c62ddd724aa802eb43c2bf
Steven Odhiambo <60733003+Sephens@users.noreply.github.com>
2022-08-21 00:34:09 +0300

    Update 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,41 +1,33 @@
 #include "sort.h"
-
-/**
-*swap - the positions of two elements into an array
-*@array: array
-*@item1: array element
-*@item2: array element
-*/
-void swap(int *array, int item1, int item2)
-{
-
-	int tmp;
-
-	tmp = array[item1];
-	array[item1] = array[item2];
-	array[item2] = tmp;
-}
-/**
- * shell_sort - function that sorts an array of integers in ascending
- * order using the Shell sort algorithm, using the Knuth sequence
- * @size: size of the array
- * @array: list with numbers
- */
 void shell_sort(int *array, size_t size)
 {
-	size_t gap = 1, i, index = 0;
+	size_t kunth[1000], k = 0, j = 0, i;
+	int n, j2;
 
-	if (array == NULL || size < 2)
+	if (!array)
 		return;
-	while (gap < size / 3)
-		gap = 3 * gap + 1;
-	while (gap >= 1)
+	while (j * 3 + 1 < size)
 	{
-		for (i = gap; i < size; i++)
-			for (index = i; index >= gap &&
-			 (array[index] < array[index - gap]); index -= gap)
-				swap(array, index, index - gap);
+		kunth[k] = j * 3 + 1;
+		j = kunth[k++];
+	}
+	for (i = 0; i < k; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if ((j + kunth[k - i - 1]) > size - 1)
+				break;
+			j2 = j;
+			while (array[j2] > array[j2 + kunth[k - i - 1]])
+			{
+				n = array[j2];
+				array[j2] =  array[j2 + kunth[k - i - 1]];
+				array[j2 + kunth[k - i - 1]] = n;
+				j2 = j2 - kunth[k - i - 1];
+				if (j2 < 0)
+					break;
+			}
+		}
 		print_array(array, size);
-		gap /= 3;
 	}
 }
